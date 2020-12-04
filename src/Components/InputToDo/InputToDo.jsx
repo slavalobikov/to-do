3c5d8838fb7ddb6796ae51a3a4a56c879e652b41
lobik <lobikova12@mail.ru>
2020-12-04 15:38:21 +0300

    localStorage

---
+++ src/Components/InputToDo/InputToDo.jsx
@@ -0,0 +1,60 @@
+import React, {useState} from 'react';
+
+import s from "./InputToDo.module.css";
+import {compose} from "redux";
+import {connect} from "react-redux";
+import {AddZ} from "../../Redux/Reducers/ToDoReducer";
+
+
+const InputToDo = (props) => {
+
+    const [value, setValue] = useState('');
+
+    let IdNewMessage;
+
+    if (!props.z[0]) {
+        IdNewMessage = 1;
+    } else {
+        IdNewMessage = props.z[props.z.length - 1].id + 1;
+    }
+
+    const addToDo = () => {
+
+        if (!value) {
+            return
+        }
+
+        props.AddZ(value, IdNewMessage, IdNewMessage );
+        setValue('')
+    }
+
+    return (
+        <div>
+
+               <div  className={s.addField}>
+                    <input type="text"
+                           value={value}
+                           onChange={(e) => {setValue(e.currentTarget.value)}}
+                           placeholder={'Введите свою задачу'}
+
+                    />
+                    <button onClick={() => {addToDo()}}  className={s.addBtn}>+</button>
+                </div>
+
+
+        </div>
+    );
+};
+let mapStateToProps = (state) => {
+    return {
+        z: state.ToDoReducer.z,
+
+    }
+};
+
+export default compose(
+    connect(mapStateToProps, {
+        AddZ,
+    }),
+) (InputToDo);
+
